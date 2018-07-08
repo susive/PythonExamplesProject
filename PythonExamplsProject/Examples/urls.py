40e1de9ba97ebaaf0a82a1d7758661b2a5ebd635
sagarreddyg <gantasagarreddy467@gmail.com>
2018-07-08 20:04:15 +0530

    Fav icon is added

--- PythonExamplsProject/Examples/urls.py
+++ PythonExamplsProject/Examples/urls.py
@@ -4,4 +4,7 @@ app_name = 'Examples'
 urlpatterns = [
     url(r"^$", views.HomePage.as_view(), name="all"),
     url(r"^h1/$", views.hello.as_view(), name="h1"),
+    url(r"^sumofNumbers/$", views.sumodNum.as_view(), name="sum"),
+    url(r"^even/$", views.Even.as_view(), name="even"),
+
 ]
--- PythonExamplsProject/Examples/views.py
+++ PythonExamplsProject/Examples/views.py
@@ -1,9 +1,19 @@
 from django.views.generic import TemplateView
 
 
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+
 class hello(TemplateView):
     template_name = 'helloworld/h1.html'
 
 
-class HomePage(TemplateView):
-    template_name = 'index.html'
+class sumodNum(TemplateView):
+    template_name = 'AddofNum/addofnum.html'
+
+
+class Even(TemplateView):
+    template_name = 'Evennumbers/even.html'
+
